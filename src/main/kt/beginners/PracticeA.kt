8102d4716b52122c114ff88aba666d29dc8dec50
ykonno <konchan1129@gmail.com>
2019-09-03 13:38:32 +0900

    AtCoder 解答コード Beginners-Practice

---
+++ src/main/kt/beginners/PracticeA.kt
@@ -0,0 +1,19 @@
+package kt.beginners
+
+/**
+ * @author ykonno.
+ * @since 2019/09/03.
+ */
+fun main(args: Array<String>) {
+    val a = readLine()!!.toInt()
+    val (b, c) = readLine()!!.split(" ")
+        .map(String::toInt)
+    val s = readLine()!!
+    solver(a, b, c, s)
+
+}
+
+fun solver(a: Int, b:Int, c: Int, s: String ) {
+    print(a + b + c)
+    println(" " + s)
+}
